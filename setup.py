e76e1fe43bfeda5bd25ed5e805206d8251f82b5d
tranhlok <tranhlok@gmail.com>
2023-11-02 15:54:50 -0400

    initial file structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project aims to explore, adapt, and develop a predictive system for anticipating viewers' gaze directions within immersive 360 degree images and videos by analyzing the past view direction.',
+    author='Loc Tran & Taishan Zhao',
+    license='MIT',
+)
